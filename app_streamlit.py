26973cd0798c4eec9a98f6c432926ecd21fd4de2
Yuri Braga <49165713+bragayuri@users.noreply.github.com>
2025-05-15 18:54:04 +0100

    download from google

--- app_streamlit.py
+++ app_streamlit.py
@@ -41,23 +41,17 @@ choice = st.sidebar.radio("Go to", pages)
 
 # 3. LOAD DATA AND MODEL
 @st.cache_resource
-
-
 def load_model():
-    IS_CLOUD = os.environ.get("STREAMLIT_SERVER_HEADLESS") == "true"
     local_path = "car_price_stacked_pipeline.pkl"
+    file_id = "1cpMyNSxLTBVixk-2BrjBlZP8nDd6-6YH"
+    url = f"https://drive.google.com/uc?id={file_id}"
 
-    if IS_CLOUD:
-        file_id = "1cpMyNSxLTBVixk-2BrjBlZP8nDd6-6YH"
-        url = f"https://drive.google.com/uc?id={file_id}"
-        if not os.path.exists(local_path):
-            with st.spinner("🔽 Baixando modelo do Google Drive via gdown..."):
-                gdown.download(url, local_path, quiet=False)
-                st.write("✅ Modelo baixado com sucesso.")
-                st.write(f"Tamanho: {os.path.getsize(local_path)} bytes")
-
+    # Se já baixou uma vez, não baixa de novo
     if not os.path.exists(local_path) or os.path.getsize(local_path) < 1000000:
-        raise RuntimeError("🚨 Modelo corrompido ou não baixado corretamente")
+        with st.spinner("🔽 Baixando modelo do Google Drive via gdown..."):
+            gdown.download(url, local_path, quiet=False)
+            st.success("✅ Modelo baixado com sucesso.")
+            st.info(f"Tamanho: {os.path.getsize(local_path)} bytes")
 
     return joblib.load(local_path)
 
